fc5678eaef39d72d72a16919d81c7e1f4d4fa5ee
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2023-02-21 00:25:55 -0300

    Add key in list case example

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key key,
     @required this.transaction,
@@ -11,6 +13,26 @@ class TransactionItem extends StatelessWidget {
 
   final Transaction transaction;
   final Function deleteTransaction;
+
+  @override
+  State<TransactionItem> createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color _bgColor;
+
+  @override
+  void initState() {
+    const availableColors = [
+      Colors.red,
+      Colors.black,
+      Colors.blue,
+      Colors.purple,
+    ];
+
+    _bgColor = availableColors[Random().nextInt(4)];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,26 +44,28 @@ class TransactionItem extends StatelessWidget {
       ),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
           radius: 30,
           child: Padding(
             padding: const EdgeInsets.all(6),
             child: FittedBox(
-              child: Text('\$${transaction.amount}'),
+              child: Text('\$${widget.transaction.amount}'),
             ),
           ),
         ),
         title: Text(
-          transaction.title,
+          widget.transaction.title,
           style: Theme.of(context).textTheme.headline6,
         ),
         subtitle: Text(
-          DateFormat.yMMMd().format(transaction.date),
+          DateFormat.yMMMd().format(widget.transaction.date),
         ),
         trailing: MediaQuery.of(context).size.width > 460
             ? TextButton.icon(
                 icon: const Icon(Icons.delete),
                 label: const Text('Delete'),
-                onPressed: () => deleteTransaction(transaction.id),
+                onPressed: () =>
+                    widget.deleteTransaction(widget.transaction.id),
                 style: ButtonStyle(
                   foregroundColor: MaterialStateProperty.all<Color>(
                       Theme.of(context).errorColor),
@@ -50,7 +74,8 @@ class TransactionItem extends StatelessWidget {
             : IconButton(
                 icon: const Icon(Icons.delete),
                 color: Theme.of(context).errorColor,
-                onPressed: () => deleteTransaction(transaction.id),
+                onPressed: () =>
+                    widget.deleteTransaction(widget.transaction.id),
               ),
       ),
     );
